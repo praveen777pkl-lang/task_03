9520addb28f3daf0356316c0ac2418739f8b3e32
praveen777pkl-lang <praveen777pkl@gmail.com>
2026-09-13 18:27:32 +0530

    Git Uploaded

---
+++ tuple_and_dictionary.py
@@ -0,0 +1,36 @@
+#Question 1: Tuple
+
+tuples=(60,40,26,10,88,55,30)
+
+print("Highest Number :",max(tuples))
+
+print("Lowest Number :",min(tuples))
+
+
+#Example output:
+
+      #Highest Number : 88
+      #Lowest Number : 10
+
+#Question 2: Dictionary
+
+dict1={}
+
+dict1["Name"]=input("Enter Your Name :")
+dict1["Reg_No"]=int(input("Enter Your Register Number :"))
+dict1["Class"]=input("Enter Your Class :")
+dict1["Section"]=input("Enter Your Section :")
+dict1["Total_Subject_Marks"]=int(input("Enter Your Total Subject Marks :"))
+
+print("Student Details :",dict1)
+print("Updated Successfully...")
+
+#Example output:
+         #Student Details : {
+                              # 'Name': 'PRAVEEN K',
+                              # 'Reg_No': 67,
+                              # 'Class': 'Computer Science', 
+                              # 'Section': 'B', 'Total_Subject_Marks': 455
+                              #  }
+                              #  Updated Successfully...
+
